bcd45b5f9704f6d58a949afc4d220ce7082bc6bb
Princess Anne Sharmaine I. Mañalac <dynamic.pasimanalac@gmail.com>
2015-12-18 00:48:59 +0800

    VB - Hello World
    Hello World Program using visual basic

---
+++ HelloWorld-PrincessManalac/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Label1.Text = "Hello World"
+        Button1.Visible = False
+
+
+    End Sub
+End Class
